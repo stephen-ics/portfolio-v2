6a07d760af34ba51dabb30c3c3b142a8b0c2edc1
Stephen Ni <stephenni1234@gmail.com>
2022-07-26 23:59:55 -0400

    text change

--- src/components/Hobby.js
+++ src/components/Hobby.js
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion'
 
 const Hobby = ({ title, description }) => {
   return (
-    <motion.div className='flex flex-col justify-center items-center rounded-xl w-60 h-60 bg-white mr-12 bg-gradient-to-r from-blue-400 to-purple-400'
+    <motion.div className='flex flex-col text-slate-800 justify-center items-center rounded-xl w-60 h-60 bg-white mr-12 bg-gradient-to-r from-blue-400 to-purple-400'
         whileHover={{scale:1.1}}
         whileTap={{scale:0.9}}>
         <h1 className='m-0 mb-3'>{title}</h1>
