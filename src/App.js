074926fe9a292255ecce7200cf18ef905a3185b8
stephen-ics <stephenni1234@gmail.com>
2022-08-28 16:09:29 -0400

    home page revamp

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className='h-full'>
+    <div className='h-full w-full absolute'>
       <BrowserRouter>
         <NavbarFinal/>
         <AnimatedRoutes/>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,19 @@ import { Link } from 'react-router-dom'
 import './page-styles/Home.css'
 import Logo from '../pictures/Logo.png'
 import { motion } from 'framer-motion';
+import { useState, useRef, useEffect } from 'react';
+import { faCarSide } from '@fortawesome/free-solid-svg-icons';
+import { useInView } from 'react-intersection-observer'
+
 
 export default function Home() {
+
+  const {ref, inView} = useInView();
+
+  useEffect(() => {
+    console.log()
+  })
+
 
   const dropInLeft ={
     hidden: {
@@ -29,19 +40,17 @@ export default function Home() {
 
   return (
     <motion.div 
-      className='h-full text-slate-700 flex flex-col items-center justify-center mt-32'
+      className='h-full text-slate-700 flex flex-col mt-24 w-full'
       initial={{opacity:0}}
       animate={{opacity:1, transition: {duration:0.5}}}
       exit={{opacity:0, transition: {duration:0.25}}}
     >
-      <motion.div className='flex h-full'>
-        <motion.div className='h-full flex flex-col ml-28'>
+      <motion.div className='flex justify-center'>
+        <motion.div className='flex flex-col justify-around'>
           <h2 className='text-4xl'>Hello!</h2>
-          <h1 className='text-8xl font-bold'>
-            I'm Stephen Ni
-          </h1>
-          <h2 className='text-xl mb-8'>Aspiring Full-Stack Developer</h2>
-          <div className='flex h-full'>
+          <h1 className='text-8xl font-bold'>I'm Stephen Ni</h1>
+          <h2 className='text-3xl mb-8'>Aspiring Full-Stack Developer</h2>
+          <div className='flex'>
             <Link to='/experiences'>
               <motion.button className="button"
                 whileHover={{scale:1.1}}
@@ -55,17 +64,17 @@ export default function Home() {
                   whileTap={{scale:0.9}}>
                     Projects
                 </motion.button>
-              </Link>
-            </div>
-          </motion.div>
+            </Link>
+          </div>
+        </motion.div>
         <motion.div
           whileHover={{scale:1.1}}
           whileTap={{scale:0.9}}>     
-            <img src={Logo} className='ml-40 rounded-2xl w-1/2 object-cover mr-28'/>
+            <img src={Logo} className='w-96 ml-40 rounded-full object-cover border-green-500 border-solid border-3'/>
         </motion.div>
       </motion.div>
-      <h1 className='text-5xl mt-20 mb-8 w-full flex'>React.js   Next.js   Tensorflow   Python   Java   C#   Firebase   Blockchain  AI Databases Full-Stack</h1>
       
+
     </motion.div>
   )
 }
