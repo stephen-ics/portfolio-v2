23c873a213475baacc5fe5a835db0c87d5e8247b
stephen-ics <stephenni1234@gmail.com>
2023-07-01 21:07:11 -0400

    hid modal when on mobile

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -39,29 +39,31 @@ const ModalButton = ({ onClick, label }) => (
 
 const Modal = ({ handleClose, title, subtitle, description, techStack, image, github, devpost }) => {
     return (
-        <Backdrop onClick={handleClose}>
-            <motion.div 
-                onClick={(e) => e.stopPropagation()}
-                className='modal orange-gradient'
-                variants={dropIn}
-                initial='hidden'
-                animate='visible'
-                exit='exit'
-            >
-                <img src={image} className='modal-image object-cover rounded-l-xl'/>
-                <div className='flex flex-col items-center ml-10'>
-                    <ModalButton onClick={handleClose} label='Close'></ModalButton>
-                    <div className='flex justify-between mt-10 items-start ml-4'>
-                        <div>
-                            <h1 className='text-5xl'>{title}</h1>
-                            <h3 className='text-xl'>{subtitle}</h3>
-                            <p className='text-xl mt-2'>{description}</p>
-                            <p className='text-xl mt-10'>{techStack}</p>
+        <div className='sm:block hidden'>
+            <Backdrop onClick={handleClose}>
+                <motion.div 
+                    onClick={(e) => e.stopPropagation()}
+                    className='modal orange-gradient'
+                    variants={dropIn}
+                    initial='hidden'
+                    animate='visible'
+                    exit='exit'
+                >
+                    <img src={image} className='modal-image object-cover rounded-l-xl'/>
+                    <div className='flex flex-col items-center ml-10'>
+                        <ModalButton onClick={handleClose} label='Close'></ModalButton>
+                        <div className='flex justify-between mt-10 items-start ml-4'>
+                            <div>
+                                <h1 className='text-5xl'>{title}</h1>
+                                <h3 className='text-xl'>{subtitle}</h3>
+                                <p className='text-xl mt-2'>{description}</p>
+                                <p className='text-xl mt-10'>{techStack}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </motion.div>
-        </Backdrop>
+                </motion.div>
+            </Backdrop>
+        </div>
     )
 }
 
